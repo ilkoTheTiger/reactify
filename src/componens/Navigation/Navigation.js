885040f9b161d735b997f466278bef6c35f37e8b
ilkoTheTiger <ilkothetiger@gmail.com>
2023-03-19 18:36:17 +0200

    Navigation styled

--- src/componens/Navigation/Navigation.js
+++ src/componens/Navigation/Navigation.js
@@ -1,9 +1,14 @@
-import styles from './Navigation.module.css'
+import { Link } from 'react-router-dom';
+import styles from './Navigation.module.css';
 
 export const Navigation = () => {
     return (
         <nav className={styles.navigation}>
-            <p>Navigation</p>
+            <ul>
+                <li><Link to='/'>Home</Link></li>
+                <li><Link to='/bookings'>Bookings</Link></li>
+                <li><Link to='/staff'>Service Providers</Link></li>
+            </ul>
         </nav>
     );
 };
