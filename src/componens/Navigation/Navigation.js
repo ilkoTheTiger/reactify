3df8362c5863c9aef89fdaad7b2c061e0973c812
ilkoTheTiger <ilkothetiger@gmail.com>
2023-03-19 19:48:32 +0200

    Favicon and Navigation Logo pushed to Google Drive

--- src/componens/Navigation/Navigation.js
+++ src/componens/Navigation/Navigation.js
@@ -5,7 +5,7 @@ export const Navigation = () => {
     return (
         <nav className={styles.navigation}>
             <ul>
-                <li><Link to='/'><img src='./clean-logo-100x100.png' /></Link></li>
+                <li><Link to='/'><img src="https://drive.google.com/uc?id=1Jm5CIpJz-0VdJtH4ZWbI_oLLIHOWYzAt" alt="CleanIt Logo from Google Drive" /></Link></li>
                 <li><Link to='/'><i class="fa fa-home" aria-hidden="true"></i></Link></li>
                 <li><Link to='/bookings'>Bookings</Link></li>
                 <li><Link to='/staff'>Service Providers</Link></li>
