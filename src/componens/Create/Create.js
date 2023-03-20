cf804b8272b0bc111ff830022445860315e1369a
ilkoTheTiger <ilkothetiger@gmail.com>
2023-03-20 12:44:18 +0200

    Simple Create form added

--- src/componens/Create/Create.js
+++ src/componens/Create/Create.js
@@ -4,7 +4,14 @@ export const Create = () => {
     return (
         <>
         <form class={styles.create}>
+            <div>
+                <label htmlFor='From'>From</label>
+                <input type='text' name='from' id='from' />
+            </div>
 
+            <div>
+                <input type='submit' value='Host' />
+            </div>
         </form>
         </>
     );
