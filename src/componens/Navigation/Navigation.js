271596d68c791e0e57b987e93eb8a1793fda2260
ilkoTheTiger <ilkothetiger@gmail.com>
2023-03-20 10:57:58 +0200

    Favicon and logo swapped with transparent ones

--- src/componens/Navigation/Navigation.js
+++ src/componens/Navigation/Navigation.js
@@ -5,7 +5,7 @@ export const Navigation = () => {
     return (
         <nav className={styles.navigation}>
             <ul>
-                <li><Link to='/'><img src="https://drive.google.com/uc?id=1VpaZHngfLVxhcgPS_uw4-hi2_17t7ZBn" alt="CleanIt Logo from Google Drive" /></Link></li>
+                <li><Link to='/'><img src="https://drive.google.com/uc?id=1YtESxvOdQR4C4MLY7qGLq_wAHDFvU8Xd" alt="CleanIt Logo from Google Drive" /></Link></li>
                 <li><Link to='/'><i class="fa fa-home" aria-hidden="true"></i></Link></li>
                 <li><Link to='/bookings'>Bookings</Link></li>
                 <li><Link to='/staff'>Service Providers</Link></li>
