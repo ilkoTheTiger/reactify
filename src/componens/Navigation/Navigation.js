e157c3df5189f845d5344f76c1a3819742a36e06
ilkoTheTiger <ilkothetiger@gmail.com>
2023-03-20 11:11:05 +0200

    Navigation updated

--- src/componens/Navigation/Navigation.js
+++ src/componens/Navigation/Navigation.js
@@ -7,8 +7,8 @@ export const Navigation = () => {
             <ul>
                 <li><Link to='/'><img src="https://drive.google.com/uc?id=1YtESxvOdQR4C4MLY7qGLq_wAHDFvU8Xd" alt="CleanIt Logo from Google Drive" /></Link></li>
                 <li><Link to='/'><i class="fa fa-home" aria-hidden="true"></i></Link></li>
-                <li><Link to='/bookings'>Bookings</Link></li>
-                <li><Link to='/staff'>Service Providers</Link></li>
+                <li><Link to='/commutes'>Commutes</Link></li>
+                <li><Link to='/create'>Create</Link></li>
                 <li><Link to='/login'>Login</Link></li>
                 <li><Link to='/register'>Register</Link></li>
             </ul>
