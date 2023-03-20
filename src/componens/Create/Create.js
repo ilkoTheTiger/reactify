0050839c33f612ea5b4b8eb68a2880e3797cbd1a
ilkoTheTiger <ilkothetiger@gmail.com>
2023-03-20 13:19:44 +0200

    Placeholder added to from field

--- src/componens/Create/Create.js
+++ src/componens/Create/Create.js
@@ -1,12 +1,16 @@
+import { useState } from 'react';
 import styles from './Create.module.css';
 
 export const Create = () => {
+    const [from, setFrom] = useState();
+
     return (
         <>
+        <h2>Host a Commute</h2>
         <form class={styles.create}>
             <div>
                 <label htmlFor='From'>From</label>
-                <input type='text' name='from' id='from' />
+                <input type='text' name='from' id='from' placeholder='Enter city of departure..'/>
             </div>
 
             <div>
