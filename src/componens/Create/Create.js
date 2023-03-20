d8e886778494825845a21232e32e85304925a998
ilkoTheTiger <ilkothetiger@gmail.com>
2023-03-20 14:10:22 +0200

    Seats select field added to Create Form

--- src/componens/Create/Create.js
+++ src/componens/Create/Create.js
@@ -4,6 +4,7 @@ import styles from './Create.module.css';
 export const Create = () => {
     const [from, setFrom] = useState('');
     const [to, setTo] = useState('');
+    const [seats, setSeats] = useState('1');
 
     const onFromChange = (e) => {
         setFrom(e.target.value);
@@ -13,14 +14,18 @@ export const Create = () => {
         setTo(e.target.value);
     };
 
-    const submitHandler = (e) => {
+    const onSeatsChange = (e) => {
+        setSeats(e.target.value);
+    };
+
+    const onSubmitHandler = (e) => {
         e.preventDefault();
     };
 
     return (
         <>
         <h2>Host a Commute</h2>
-        <form class={styles.create} onSubmit={submitHandler}>
+        <form class={styles.create} onSubmit={onSubmitHandler}>
             <div>
                 <label htmlFor='from'>From</label>
                 <input 
@@ -43,6 +48,16 @@ export const Create = () => {
                 value={to}
                 placeholder='Enter city of arrival..'
                 />
+            </div>
+
+            <div>
+                <label htmlFor='seats'>Seats</label>
+                <select name='seats' id='seats' value={seats} onChange={onSeatsChange}>
+                    <option value='1'>1</option>
+                    <option value='2'>2</option>
+                    <option value='3'>3</option>
+                    <option value='4'>4</option>
+                </select>
             </div>
 
             <div>
