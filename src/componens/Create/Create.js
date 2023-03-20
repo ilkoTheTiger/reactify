1916581b19fd08892623d65723fe2d56e6df51f7
ilkoTheTiger <ilkothetiger@gmail.com>
2023-03-20 13:34:52 +0200

    To field added to Create form

--- src/componens/Create/Create.js
+++ src/componens/Create/Create.js
@@ -2,15 +2,47 @@ import { useState } from 'react';
 import styles from './Create.module.css';
 
 export const Create = () => {
-    const [from, setFrom] = useState();
+    const [from, setFrom] = useState('');
+    const [to, setTo] = useState('');
+
+    const onFromChange = (e) => {
+        setFrom(e.target.value);
+    };
+
+    const onToChange = (e) => {
+        setTo(e.target.value);
+    };
+
+    // submitHandler(e) {
+    //     e.preventDefault();
+    // };
 
     return (
         <>
         <h2>Host a Commute</h2>
         <form class={styles.create}>
             <div>
-                <label htmlFor='From'>From</label>
-                <input type='text' name='from' id='from' placeholder='Enter city of departure..'/>
+                <label htmlFor='from'>From</label>
+                <input 
+                type='text' 
+                name='from' 
+                id='from' 
+                onChange={onFromChange}
+                value={from}
+                placeholder='Enter city of departure..'
+                />
+            </div>
+
+            <div>
+                <label htmlFor='to'>To</label>
+                <input 
+                type='text' 
+                name='to' 
+                id='to' 
+                onChange={onToChange}
+                value={to}
+                placeholder='Enter city of arrival..'
+                />
             </div>
 
             <div>
