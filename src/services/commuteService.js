f07076c47295d19a4695f20d74cd7185189c6cb1
ilkoTheTiger <ilkothetiger@gmail.com>
2023-03-23 01:17:50 +0200

    Remote deployment tests successful

--- src/services/commuteService.js
+++ src/services/commuteService.js
@@ -1,7 +1,7 @@
 import * as request from "./requester"
 
-// const baseUrl = 'http://localhost:3005/api/commutes'
-const baseUrl = 'https://server-production-8381.up.railway.app/api/commutes'
+// const baseUrl = 'https://server-production-8381.up.railway.app/api/commutes'
+const baseUrl = 'http://localhost:3005/api/commutes'
 
 export const getAll = async () => {
     const result = await request.get(baseUrl);
