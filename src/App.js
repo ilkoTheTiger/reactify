4ea5347b9b9575bb85347001c315628bd1921791
ilkoTheTiger <ilkothetiger@gmail.com>
2023-03-22 09:57:59 +0200

    Catalog component and Create handler added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, useNavigate } from 'react-router-dom';
 import './App.css';
 import { Host } from './componens/Host/Host';
 import { Footer } from './componens/Footer/Footer';
@@ -6,7 +6,18 @@ import { Footer } from './componens/Footer/Footer';
 import { Home } from './componens/Home/Home'
 import { Navigation } from './componens/Navigation/Navigation';
 
+
+
 function App() {
+    const navigate = useNavigate();
+
+    const onCreateGameSubmit = async (data) => {
+        const newGame = await commuteService.create(data);
+    
+        setGames(state => [...state, newGame]);
+    
+        navigate('/catalog');
+    };
 
     return (
         <>
@@ -16,7 +27,10 @@ function App() {
             <main>
                 <Routes>
                     <Route path='/' element={<Home />} />
-                    <Route path='/host' element={<Host />} />
+                    <Route path='/host' element={<Host onCreateGameSubmit={onCreateGameSubmit} />} />
+                    <Route path='/catalog' element={<Catalog />} />
+                    <Route path='/login' element={<h1>Login Page</h1>} />
+                    <Route path='/register' element={<h1>Register Page</h1>} />
                     <Route path='/*' element={<h1>404 Page</h1>} />
                 </Routes>
             </main>
