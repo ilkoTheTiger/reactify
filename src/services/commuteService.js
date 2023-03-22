e7a91b95f5b03832874cff4967954b9e4b2e3fec
ilkoTheTiger <ilkothetiger@gmail.com>
2023-03-22 12:05:23 +0200

    REST API endpoint fixed in commuteService on client

--- src/services/commuteService.js
+++ src/services/commuteService.js
@@ -1,6 +1,6 @@
 import * as request from "./requester"
 
-const baseUrl = 'http://localhost:3005/'
+const baseUrl = 'http://localhost:3005/api/commutes'
 
 export const create = async (commuteData) => {
     const result = await request.post(baseUrl, commuteData);
