480e4e80b3161ff0bb1f0a02e2430049a4bc809b
ilkoTheTiger <ilkothetiger@gmail.com>
2023-03-28 10:51:56 +0300

    Active links added

--- src/componens/Navigation/Navigation.js
+++ src/componens/Navigation/Navigation.js
@@ -1,16 +1,16 @@
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import styles from './Navigation.module.css';
 
 export const Navigation = () => {
     return (
         <nav className={styles.navigation}>
             <ul>
-                <li><Link to='/'><img src="https://drive.google.com/uc?id=1YtESxvOdQR4C4MLY7qGLq_wAHDFvU8Xd" alt="CleanIt Logo from Google Drive" /></Link></li>
-                <li><Link to='/'><i className="fa fa-home" aria-hidden="true"></i></Link></li>
-                <li><Link to='/commutes'>Commutes</Link></li>
-                <li><Link to='/host'>Host</Link></li>
-                <li><Link to='/login'>Login</Link></li>
-                <li><Link to='/register'>Register</Link></li>
+                <li><NavLink style={({isActive}) => ({color: isActive ? 'red' : 'blue'})} to='/'><img src="https://drive.google.com/uc?id=1YtESxvOdQR4C4MLY7qGLq_wAHDFvU8Xd" alt="CleanIt Logo from Google Drive" /></NavLink></li>
+                <li><NavLink style={({isActive}) => ({color: isActive ? 'red' : 'blue'})} to='/'><i className="fa fa-home" aria-hidden="true"></i></NavLink></li>
+                <li><NavLink style={({isActive}) => ({color: isActive ? 'red' : 'blue'})} to='/commutes'>Commutes</NavLink></li>
+                <li><NavLink style={({isActive}) => ({color: isActive ? 'red' : 'blue'})} to='/host'>Host</NavLink></li>
+                <li><NavLink style={({isActive}) => ({color: isActive ? 'red' : 'blue'})} to='/login'>Login</NavLink></li>
+                <li><NavLink style={({isActive}) => ({color: isActive ? 'red' : 'blue'})} to='/register'>Register</NavLink></li>
             </ul>
         </nav>
     );
