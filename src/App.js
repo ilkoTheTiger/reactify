b298e239a035a949e721581db1b6b3c89eebaa6d
ilkoTheTiger <ilkothetiger@gmail.com>
2023-03-30 14:21:36 +0300

    Login implemented

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react';
 
+import { AuthContext } from './contexts/AuthContext';
 import * as commuteService from './services/commuteService';
 
 import { Routes, Route, useNavigate } from 'react-router-dom';
@@ -11,11 +12,13 @@ import { Home } from './componens/Home/Home'
 import { Navigation } from './componens/Navigation/Navigation';
 import { Catalog } from './componens/Catalog/Catalog';
 import { CommuteDetails } from './componens/CommuteDetails/CommuteDetails';
-
+import { Login } from "./components/Login/Login";
 
 function App() {
     const navigate = useNavigate();
     const [commutes, setCommutes] = useState([]);
+    const [auth, setAuth] = useState({});
+
     useEffect(() => {
         commuteService.getAll()
             .then(result => {
@@ -27,27 +30,33 @@ function App() {
         const createdCommute = await commuteService.create(data);
 
         setCommutes(state => [...state, createdCommute.commute]);
-    
+
         navigate('/commutes');
     };
 
+    const onLoginSubmit = async (data) => {
+        console.log(data);
+    }
+
     return (
         <>
-            <header>
-                <Navigation />
-            </header>
-            <main>
-                <Routes>
-                    <Route path='/' element={<Home />} />
-                    <Route path='/host' element={<Host onHostCommuteSubmit={onHostCommuteSubmit} />} />
-                    <Route path='/commutes' element={<Catalog commutes={commutes}/>} />
-                    <Route path='/commutes/:commuteId' element={<CommuteDetails />}></Route>
-                    <Route path='/login' element={<h1>Login Page</h1>} />
-                    <Route path='/register' element={<h1>Register Page</h1>} />
-                    <Route path='/*' element={<h1>404 Page</h1>} />
-                </Routes>
-            </main>
-            <Footer />
+            <AuthContext.Provider value={{ onLoginSubmit }}>
+                <header>
+                    <Navigation />
+                </header>
+                <main>
+                    <Routes>
+                        <Route path='/' element={<Home />} />
+                        <Route path='/host' element={<Host onHostCommuteSubmit={onHostCommuteSubmit} />} />
+                        <Route path='/commutes' element={<Catalog commutes={commutes} />} />
+                        <Route path='/commutes/:commuteId' element={<CommuteDetails />}></Route>
+                        <Route path='/login' element={<Login />} />
+                        <Route path='/register' element={<h1>Register Page</h1>} />
+                        <Route path='/*' element={<h1>404 Page</h1>} />
+                    </Routes>
+                </main>
+                <Footer />
+            </AuthContext.Provider>
         </>
     );
 }
