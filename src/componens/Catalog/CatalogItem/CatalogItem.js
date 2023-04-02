056b59f1018f1aa87169fac8017703272c80aa31
ilkoTheTiger <ilkothetiger@gmail.com>
2023-04-02 21:22:55 +0300

    Catalog styles separated with CataloGItem styles

--- src/componens/Catalog/CatalogItem/CatalogItem.js
+++ src/componens/Catalog/CatalogItem/CatalogItem.js
@@ -1,4 +1,4 @@
-import styles from '../Catalog.module.css';
+import styles from './CatalogItem.module.css';
 import { Link } from "react-router-dom";
 import { formatDate } from '../../../utils/dateUtils';
 
