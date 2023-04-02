e0db515d78d32d708b20ebd319eafe08a725514f
ilkoTheTiger <ilkothetiger@gmail.com>
2023-04-02 21:16:24 +0300

    CommuteService refactored

--- src/services/commuteService.js
+++ src/services/commuteService.js
@@ -1,22 +1,28 @@
 import * as request from "./requester"
 
 // const baseUrl = 'https://server-production-8381.up.railway.app/api/commutes'
-const baseUrl = 'http://localhost:3005/api/commutes'
+const baseUrl = 'http://localhost:3030/jsonstore/commutes'
 
 export const getAll = async () => {
     const result = await request.get(baseUrl);
-    const commutes = result.commutes;
+    const commutes = Object.values(result);
     return commutes;
 };
 
 export const getOne = async (commuteId) => {
     const result = await request.get(`${baseUrl}/${commuteId}`);
     
-    return result.commute;
+    return result;
 };
 
 export const create = async (commuteData) => {
     const result = await request.post(baseUrl, commuteData);
 
     return result;
-}
+};
+
+export const addComment = async (gameId, data) => {
+    const result = await request.post(`${baseUrl}/${gameId}/comments`, data);
+
+    return result;
+};
