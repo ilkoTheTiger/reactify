0af319f40be1e242870a9d1189dabc9d9403f3df
ilkoTheTiger <ilkothetiger@gmail.com>
2023-04-05 11:56:04 +0300

    Email fields styled

--- src/componens/Register/Register.js
+++ src/componens/Register/Register.js
@@ -26,13 +26,15 @@ export const Register = () => {
         const target = e.target.name;
         const errors = { ...formErrors }
 
-        if (target === [FormKeys.Email] && (value.length < 3 || value.length > 26)) {
-            errors.email = 'Email must be between 3 and 26 letters!';
-        } else if (target === [FormKeys.Email] && (/^[A-Za-z0-9_.]+@[A-Za-z]+.[A-Za-z]{2,3}$/.test(value))) {
+        if (target === 'email' && (value.length < 3 || value.length > 26)) {
+            errors.email = 'Email must be between 3 and 26 characters!';
+        } else if (target === 'email' && !(/^[A-Za-z0-9_.]+@[A-Za-z]+.[A-Za-z]{2,3}$/.test(value))) {
             errors.email = 'Enter a valid Email address!';
-        } else if (target === [FormKeys.Email] && !(value.length < 3 || value.length > 26)) {
+        } else if (target === 'email' && !(value.length < 3 || value.length > 26)) {
             errors.email = ''
         }
+
+        console.log(/^[A-Za-z0-9_.]+@[A-Za-z]+.[A-Za-z]{2,3}$/.test(values.email));
 
         setFormErrors(errors);
     };
@@ -40,51 +42,57 @@ export const Register = () => {
     return (
         <section id="register-page" className={styles.registerContainer}>
             <h2>Register</h2>
-            <form id="register" method="POST" onSubmit={onSubmit}>
-                <div className="container">
-                    <div className="brand-logo"></div>
-                    <h1>Register</h1>
-
+            <form className={styles.register} method="POST" onSubmit={onSubmit}>
+                <div className={styles.formRow}>
                     <label htmlFor="email">Email:</label>
-                    <input
-                        type="email"
-                        id="email"
-                        placeholder="Enter your email.."
-                        name={FormKeys.Email}
-                        value={values[FormKeys.Email]}
-                        onChange={changeHandler}
-                        onBlur={formValidate}
-                    />
+                    <div className={styles.formInput}>
+                        <span style={formErrors.email ? { borderColor: 'red' } : (values.email.length > 2 && /^[A-Za-z0-9_.]+@[A-Za-z]+.[A-Za-z]{2,3}$/.test(values.email)) ? { borderColor: 'green' } : {}}><i className='fa-solid fa-at'></i></span>
+                        <input
+                            type="email"
+                            id="email"
+                            placeholder="Enter your email.."
+                            name={FormKeys.Email}
+                            value={values[FormKeys.Email]}
+                            onChange={changeHandler}
+                            onBlur={formValidate}
+                            style={formErrors.email ? { borderColor: 'red' } : (values.email.length > 2 && /^[A-Za-z0-9_.]+@[A-Za-z]+.[A-Za-z]{2,3}$/.test(values.email)) ? { borderColor: 'green' } : {}}
+                        />
+                    </div>
+                    {formErrors.email &&
+                        <p className={styles.formError}>
+                            {formErrors.email}
+                        </p>
+                    }
+                </div>
+                <label htmlFor="pass">Password:</label>
+                <input
+                    type="password"
+                    id="register-password"
+                    placeholder="Enter your password.."
+                    name={FormKeys.Password}
+                    value={values[FormKeys.Password]}
+                    onChange={changeHandler}
+                    onBlur={formValidate}
+                />
 
-                    <label htmlFor="pass">Password:</label>
-                    <input
-                        type="password"
-                        id="register-password"
-                        placeholder="Enter your password.."
-                        name={FormKeys.Password}
-                        value={values[FormKeys.Password]}
-                        onChange={changeHandler}
-                        onBlur={formValidate}
-                    />
+                <label htmlFor="con-pass">Confirm Password:</label>
+                <input
+                    type="password"
+                    id="confirm-password"
+                    placeholder="Repeat your password.."
+                    name={FormKeys.ConfirmPassword}
+                    value={values[FormKeys.ConfirmPassword]}
+                    onChange={changeHandler}
+                    onBlur={formValidate}
+                />
 
-                    <label htmlFor="con-pass">Confirm Password:</label>
-                    <input
-                        type="password"
-                        id="confirm-password"
-                        placeholder="Repeat your password.."
-                        name={FormKeys.ConfirmPassword}
-                        value={values[FormKeys.ConfirmPassword]}
-                        onChange={changeHandler}
-                        onBlur={formValidate}
-                    />
-
-                    <input className="btn submit" type="submit" value="Register" />
-
+                <input className="btn submit" type="submit" value="Register" />
+                <div>
                     <p className="field">
                         <span>If you already have profile click <Link to="/login">here</Link></span>
                     </p>
                 </div>
             </form>
-        </section>
+        </section >
     );
 };
