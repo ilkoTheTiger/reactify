a46727407fcda974813355c9887e1a2bfac2cee4
ilkoTheTiger <ilkothetiger@gmail.com>
2023-04-08 00:28:10 +0300

    LatestCommutes styled as those in the catalog

--- src/componens/Home/Home.js
+++ src/componens/Home/Home.js
@@ -11,7 +11,7 @@ export const Home = () => {
                 <img className={styles.backgroundImage} src="https://drive.google.com/uc?id=1JonN6UroNZidwcARZb2b39WB6agyZMZ0" alt="MetniMe 404 Not Found" />
                 <h3 className={styles.homeMessage}>Welcome to MetniMe!</h3>
                 <div className={styles.homeContainer}>
-                    <h2>Latest Commutes</h2>
+                    <h2 className={styles.latestCommutes}>Latest Commutes</h2>
                     <article className={styles.cardsContainer}>
                         <div className={styles.commutesContainer}>
                             {latest.map(commute => <LatestCommute key={commute._id} {...commute} />)}
