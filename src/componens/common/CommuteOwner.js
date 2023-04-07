cbe6bfa14bbb8997e78d744f21a6d24d126687b5
ilkoTheTiger <ilkothetiger@gmail.com>
2023-04-07 13:45:22 +0300

    CommuteOwner guard added

---
+++ src/componens/common/CommuteOwner.js
@@ -0,0 +1,20 @@
+import { Navigate, Outlet, useParams } from "react-router-dom";
+
+import { useCommuteContext } from "../../contexts/CommuteContext";
+import { useAuthContext } from "../../contexts/AuthContext";
+
+export const Commute = ({
+    children,
+}) => {
+    const { commuteId } = useParams();
+    const { getCommute } = useCommuteContext();
+    const { userId } = useAuthContext();
+
+    const currentCommute = getCommute(commuteId);
+
+    if (currentCommute && currentCommute._ownerId !== userId) {
+        return <Navigate to={`/commutes/${commuteId}`} replace />
+    };
+
+    return children ? children : <Outlet />
+};
