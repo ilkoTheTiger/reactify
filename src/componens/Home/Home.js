898670294b907ae26efc51f2e221102dc672ee36
ilkoTheTiger <ilkothetiger@gmail.com>
2023-04-07 22:45:03 +0300

    Latest commutes styled appropriately in home component

--- src/componens/Home/Home.js
+++ src/componens/Home/Home.js
@@ -7,23 +7,48 @@ export const Home = () => {
             <img className={styles.backgroundImage} src="https://drive.google.com/uc?id=1JonN6UroNZidwcARZb2b39WB6agyZMZ0" alt="MetniMe 404 Not Found" />
             <div className={styles.homeContainer}>
                 <h1>Latest Commutes</h1>
-
-                {/* <!-- Display div: with information about every game (if any) --> */}
-                <div className="game">
-                    <div className="image-wrap">
-                        <img src="./images/CoverFire.png" />
+                <article className={styles.cardsContainer}>
+                    {/* <!-- Display div: with information about every game (if any) --> */}
+                    <div className={styles.commuteCard}>
+                        <div className="image-wrap">
+                            <img src="./images/CoverFire.png" />
+                        </div>
+                        <h3>Cover Fire</h3>
+                        <div className="rating">
+                            <span>☆</span><span>☆</span><span>☆</span><span>☆</span><span>☆</span>
+                        </div>
+                        <div className="data-buttons">
+                            <a href="#" className="btn details-btn">Details</a>
+                        </div>
                     </div>
-                    <h3>Cover Fire</h3>
-                    <div className="rating">
-                        <span>☆</span><span>☆</span><span>☆</span><span>☆</span><span>☆</span>
+                    <div className={styles.commuteCard}>
+                        <div className="image-wrap">
+                            <img src="./images/CoverFire.png" />
+                        </div>
+                        <h3>Cover Fire</h3>
+                        <div className="rating">
+                            <span>☆</span><span>☆</span><span>☆</span><span>☆</span><span>☆</span>
+                        </div>
+                        <div className="data-buttons">
+                            <a href="#" className="btn details-btn">Details</a>
+                        </div>
                     </div>
-                    <div className="data-buttons">
-                        <a href="#" className="btn details-btn">Details</a>
+                    <div className={styles.commuteCard}>
+                        <div className="image-wrap">
+                            <img src="./images/CoverFire.png" />
+                        </div>
+                        <h3>Cover Fire</h3>
+                        <div className="rating">
+                            <span>☆</span><span>☆</span><span>☆</span><span>☆</span><span>☆</span>
+                        </div>
+                        <div className="data-buttons">
+                            <a href="#" className="btn details-btn">Details</a>
+                        </div>
                     </div>
-                </div>
-
+                    
+                </article>
                 {/* <!-- Display paragraph: If there is no games  --> */}
-                <p className="no-articles">No games yet</p>
+                <p className="no-articles">No commutes available right now!</p>
             </div>
         </section>
     );
