d53278b372f080f10091eb0623e019cdd826b947
Ilia Dimchev <66043963+ilkoTheTiger@users.noreply.github.com>
2023-05-16 18:12:20 +0100

    Update Navigation.js
    Webhook test

--- src/componens/Navigation/Navigation.js
+++ src/componens/Navigation/Navigation.js
@@ -12,7 +12,7 @@ export const Navigation = () => {
             <ul>
                 <li><NavLink style={({ isActive }) => ({ color: isActive ? '#13bc9c' : '' })} to='/'><img src="https://drive.google.com/uc?id=1YtESxvOdQR4C4MLY7qGLq_wAHDFvU8Xd" alt="CleanIt Logo from Google Drive" /></NavLink></li>
                 <li><NavLink style={({ isActive }) => ({ color: isActive ? '#13bc9c' : '' })} to='/'><i className="fa fa-home" aria-hidden="true"></i></NavLink></li>
-                <li><NavLink style={({ isActive }) => ({ color: isActive ? '#13bc9c' : '' })} to='/commutes'>Commutes</NavLink></li>
+                <li><NavLink style={({ isActive }) => ({ color: isActive ? '#13bc9c' : '' })} to='/commutes'>Rides</NavLink></li>
                 {isAuthenticated && (
                     <>
                         <li><NavLink style={({ isActive }) => ({ color: isActive ? '#13bc9c' : '' })} to='/host'>Host</NavLink></li>
